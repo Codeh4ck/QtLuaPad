8d13551dc1817e79d9061e490d25b69d0109ee0f
nikolas1994 <nikolas1994@53c863e7-f60d-dbc7-0831-442f4b67b08d>
2010-09-25 02:26:17 +0000

    Committed changes.

---
+++ luaeditor.h
@@ -0,0 +1,47 @@
+#ifndef LUAEDITOR_H
+#define LUAEDITOR_H
+
+#include <QtGui>
+#include <Qsci/qsciscintilla.h>
+#include <Qsci/qscilexerlua.h>
+
+class MainWindow;
+
+class LuaEditor : public QsciScintilla
+{
+    Q_OBJECT
+public:
+    LuaEditor();
+
+    static LuaEditor *getInstance()
+    {
+        static LuaEditor e;
+        return &e;
+    }
+
+    void initLexer();
+    QsciLexerLua *getLexer() { return lexer; }
+    void newFile();
+    bool openFile(const QString &file);
+    bool save();
+    bool saveAs();
+    bool saveFile(const QString &name);
+    QString currentFileP() { return currentFile; }
+
+protected:
+    void closeEvent(QCloseEvent *event);
+
+private slots:
+    void setDocumentModified();
+
+private:
+    QsciLexerLua *lexer;
+    bool isUntitled;
+    QString currentFile;
+    QMessageBox::StandardButton askToSave();
+    void setCurrentFile(const QString &file);
+    QString getStrippedName(const QString &fullPath);
+    QString getCurrentFile();
+};
+
+#endif // LUAEDITOR_H
