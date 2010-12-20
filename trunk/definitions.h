1800c3b34545c9f67e1287798141ac86c0f5ca24
F.Fallen45 <F.Fallen45@53c863e7-f60d-dbc7-0831-442f4b67b08d>
2010-12-20 15:48:14 +0000

    * added more defintions to defintions.h

--- trunk/definitions.h
+++ trunk/definitions.h
@@ -1,12 +1,72 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
-#endif // DEFINITIONS_H
-
 #define APPVRSN "1.0"
 #define APPNAME "QtLuaPad"
 #define ORGNAME "Delirium"
 #define ORGURL "http://otland.net/forum.php"
 
+#ifdef assert
+	#undef assert
+#endif
 
-//More definitions will be added.
+#include <sstream>
+#include <stdint.h>
+
+#ifdef qApp
+	#define assert(x)\
+		if(!x)\
+		{\
+			std::stringstream ss;\
+			ss << "Assertaion to: " << #x << " failed. line: " << __LINE__ << ", file: " << __FILE__ << ". Continue?";\
+			uint32_t ret = QMessageBox::critical(NULL, tr("Error"), tr(ss.str().c_str()),\
+				QMessageBox::Ok | QMessageBox::Cancel);\
+			switch (ret)\
+			{\
+				case QMessageBox::Ok:\
+					break;\
+				\
+				case QMessageBox::Cancel:\
+					qApp->quit();\
+			}\
+		}
+#endif
+
+#define QTLUAPAD_WARNING(x, y)\
+	std::stringstream ss;\
+	ss << "[Warning - " << #x << "] " << #y << ".";\
+	QMessageBox::warning(NULL, tr("Warning"), tr(ss.str().c_str()));
+
+#define QTLUAPAD_ERROR(x, y)\
+	std::stringstream ss;\
+	ss << "[Error - " << #x << "] " << #y << ".";\
+	QMessageBox::critical(NULL, tr("Error"), tr(ss.str().c_str()));
+
+#define QTLUAPAD_NOTICE(x, y)\
+	std::stringstream ss;\
+	ss << "[Notice - " << #x << "] " << #y << ".";\
+	QMessageBox::information(NULL, tr("Notice"), tr(ss.str().c_str()));
+
+template<typename __T>
+inline void*
+__construct(__T* __p)
+{ return ::new(static_cast<void*>(__p)) __T(); }
+
+template<typename __T>
+inline void
+__destruct(__T* __p)
+{ assert(__p != 0); __p->~__T(); }
+
+typedef size_t size_type;
+
+template<typename __T>
+inline __T*
+__allocate(size_type __n, const void* = 0)
+{ return static_cast<__T*>(::operator new(__n * sizeof(__T*))); }
+
+template<typename __T>
+inline void
+__deallocate(__T* __p)
+{ ::operator delete(__p); }
+
+#endif // DEFINITIONS_H
